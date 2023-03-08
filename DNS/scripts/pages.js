d828023b155ea7d5ba026d9aeaa6b0f477b6b5f8
Andre E. dos Santos <andre.eds@gmail.com>
2023-03-08 01:52:24 -0700

    New placeholder

--- DNS/scripts/pages.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-export function getPage(pageName) {
-    var pages = {
-        "help": ["help_1", "help_2", "help_3", "help_4", "help_5", "help_6"],
-        "credits": ["credits", "about"],
-    }
-    return pages[pageName];
-}
--- adi_game/README.md
+++
@@ -1,7 +0,0 @@
-# phaser-platform
-
-![phaser-platform](https://raw.github.com/felipepucinelli/phaser-platform/master/demo.jpg)
-
-A platform game developed with phaser.io
-
-[Play the game](http://felipepucinelli.github.io/phaser-platform/)
